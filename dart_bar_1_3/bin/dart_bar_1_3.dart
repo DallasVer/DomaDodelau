fd323df2252c11491ea8996ac58aafcdd1247f8a
DallasVer <118686739+DallasVer@users.noreply.github.com>
2022-11-30 10:04:23 +0700

    Add files via upload

---
+++ dart_bar_1_3/bin/dart_bar_1_3.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> arguments) {
+  List <int> list = List.generate(5, (a) => 1+Random().nextInt(10 - 1));
+  for (int f = 0; f < list.length; f++) {
+   for (var i = 0; i < list.length-1; i++){
+    if (num[i] == )
+    list.indexOf(i);
+    //  var c = 0;
+    //  if (list[i] > list[i + 1] ){
+    //    c = list[i];
+    //    list[i] = list[i + 1];
+    //    list[i + 1] = c;
+    //  }
+   } 
+   print(list );
+ }
+}
---
+++ dart_bar_1_3/test/dart_bar_1_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_bar_1_3/dart_bar_1_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
